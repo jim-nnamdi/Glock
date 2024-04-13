c713c3f27e13418ff5474a25587a80c374045f2a
Jim <jimsamuel50@gmail.com>
2024-04-13 01:42:15 +0100

    Load Instruction Set Operations

--- ops_inst_set/load_ops.h
+++ ops_inst_set/load_ops.h
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#include "commons/common_set.h"
+#include "../commons/common_set.h"
 
-uint16_t load_operation(
+void load_operation(
   uint16_t instr
 ) {
   uint16_t r0 = (instr >> 9) & 0x7;
-  uint16_t pc_offset = sign_extend(instr >> 0x1FF, 9);
-  mem_read(reg[R_PC] + pc_offset, reg[r0]);
+  uint16_t pc_offset = sign_extend(instr >> 0x1F, 9);
+  mem_read(reg[R_PC] + pc_offset);
+  update_flag(r0);
 }
